c4acb73820bc99071014a37622b544474a785d70
ImanBanerjee <imanbanerjee03@gmail.com>
2024-02-16 13:26:25 +0530

    Lesson 1 Lesson 2

---
+++ src/lessontwo.java
@@ -0,0 +1,28 @@
+public class lessontwo {
+
+    /* HOW TO READ CODE / Java Syntax */
+
+    // Code is read from top to bottom
+
+    // This is a single line Comment
+
+    /*
+     *
+     * This is a
+     * Multiline Comment!
+     *
+     * */
+
+    // Declaring a Variable | Note that all lines end with a semi-colon!
+    int i;
+
+    // Assigning a Variable
+    // i = 30;
+
+    // Declaring and Assigning (Initializing)
+    int x = 10;
+
+    // Access variables/method of Objects
+    String s = "Hello World";
+        s.length();
+}
